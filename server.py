efe2022f2350f32fd67ef37b1c18281f5d907e2b
Bailey <bailey.tincher@btin.io>
2019-03-08 12:52:15 -0600

    Fix front end

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, FloatField, TextAreaField, validators, StringField, SubmitField
 import os
 
 # App config.
@@ -8,25 +8,34 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
-class ReusableForm(Form):
-    handle = TextField('Handle', validators=[validators.required()])
+class SetupForm(Form):
+    handle = TextField('handle', validators=[validators.required()])
+    selectorRange = FloatField('selectorRange', validators=[validators.required()])
+    generate = SubmitField('generate')
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    form = ReusableForm(request.form)
+    setupForm = SetupForm(request.form)
+    # generateForm = GenerateForm(request.form)
 
-    print(form.errors)
+    print(setupForm.errors)
     if request.method == 'POST':
         handle = request.form['handle']
+        range = request.form['selectorRange']
         print(handle)
+        print(range)
 
-    if form.validate():
+        tweet = "UIUC ADSA tweet generator using markov chains."
+
+    if setupForm.validate():
         # Save the comment here.
-        flash(handle)
+        flash(handle, 'handleName')
+        flash(tweet, 'tweet')
     else:
-        flash('ADSA_EOH_2019 ')
+        flash('ADSA_EOH_2019 ', 'handleName')
+        flash("UIUC ADSA tweet generator using markov chains.", 'tweet')
 
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=setupForm)
 
 if __name__ == "__main__":
     app.run()
