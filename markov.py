e409e0af6b09e5ace6588da1d90d871ac67c4184
Rohan Tikmany <tikmanyrohan97@gmail.com>
2019-03-08 10:47:37 -0600

    Add ability to keep track of starting words
    Change from length of the the snippet to
    no of sentences thereby making sure we
    end with a punctuation mark

--- markov.py
+++ markov.py
@@ -1,9 +1,11 @@
-#Import libraries.
+# Import libraries.
 import sys
 import random
 import numpy as np
 import nltk
-import re, json
+import re
+import json
+
 
 def readdata(file):
     '''Read file and return contents.'''
@@ -18,29 +20,36 @@ def makerule(data, context):
     # words = nltk.word_tokenize(data)
     words = re.compile("\s").split(data)
     index = context
-
+    print("HERE")
     for word in words[index:]:
         key = ' '.join(words[index-context:index])
         if key not in rule:
             rule[key] = {}
 
-        if word in rule[key]:
+        if word in rule[ke]:
             rule[key][word] += 1
         else:
             rule[key][word] = 1
 
         index += 1
-
     return rule
+
 
 def makerule(textsets, weights, context):
     '''Make a rule dict for given data.'''
     rule = {}
     # words = nltk.word_tokenize(data)
     wordsets = [d.split() for d in textsets]
-
     index = context
-
+    sentsets = [nltk.sent_tokenize(d) for d in textsets]
+    start_keys = []
+    for i in sentsets:
+        for j in i:
+            word_ = nltk.word_tokenize(j)
+            try:
+                start_keys.append(" ".join(word_[:context]))
+            except:
+                pass
     total_words = sum([len(d) for d in wordsets])
 
     for i, words in enumerate(wordsets):
@@ -49,42 +58,53 @@ def makerule(textsets, weights, context):
             key = ' '.join(words[index-context:index])
             if key not in rule:
                 rule[key] = {}
-
             if word in rule[key]:
                 rule[key][word] += weights[i] * norm
             else:
                 rule[key][word] = weights[i] * norm
-
             index += 1
+    return rule, start_keys
 
-    return rule
 
-def makestring(rule, length):
+def makerule_sent(textsets, weights, context):
+    rule = {}
+    # words = nltk.word_tokenize(data)
+
+    with open("abc.txt", "w+") as fout:
+        fout.write(str(sentsets))
+    index = context
+    total_words = sum([len(d) for d in wordsets])
+
+
+def makestring(rule, start_keys, length):
     '''Use a given rule to make a string.'''
-    oldwords = random.choice(list(rule.keys())).split(' ') #random starting words
-    string = ' '.join(oldwords) + ' '
-
-    for i in range(length):
+    oldwords = random.choice(list(rule.keys())).split(
+        ' ')  # random starting words
+    string = random.choice(start_keys)+' '
+    oldwords = string.split()
+    print(oldwords)
+    while length > 0:
         try:
             key = ' '.join(oldwords)
-
             word_choices = []
             choice_distribution = []
             for word_choice, weight in rule[key].items():
                 word_choices.append(word_choice)
                 choice_distribution.append(weight)
 
-            choice_distribution = choice_distribution / np.sum(choice_distribution)
+            choice_distribution = choice_distribution / \
+                np.sum(choice_distribution)
             newword = np.random.choice(word_choices, p=choice_distribution)
             string += newword + ' '
-
+            if "." in newword or "!" in newword or "?" in newword:
+                length -= 1
             for word in range(len(oldwords)):
                 oldwords[word] = oldwords[(word + 1) % len(oldwords)]
             oldwords[-1] = newword
-
         except KeyError:
             return string
     return string
+
 
 def _parse_input(json_fi):
     datasets = []
@@ -99,8 +119,9 @@ def _parse_input(json_fi):
 
     return datasets, weights
 
+
 if __name__ == '__main__':
-    datasets, weights = _parse_input(sys.argv[1]) # json fi name
-    rule = makerule(datasets, weights, int(sys.argv[2])) # context
-    string = makestring(rule, int(sys.argv[3])) #length
+    datasets, weights = _parse_input(sys.argv[1])  # json fi name
+    rule, start_keys = makerule(datasets, weights, int(sys.argv[2]))  # context
+    string = makestring(rule, start_keys, int(sys.argv[3]))  # length
     print(string)
